87c0354034d434e3cfbb6b438704ffb7263cedd4
JasperC <your-jasperc-email@example.com>
2026-03-16 16:01:53 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+INSTALL_DIR="/usr/local/bin"
+SCRIPT_NAME="jas"
+
+echo "[+] Installing Jas Toolbox..."
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Run with sudo."
+   exit 1
+fi
+
+echo "[+] Installing dependencies..."
+
+apt update
+apt install -y whois curl dnsutils fastfetch secure-delete chkrootkit openssl
+
+echo "[+] Downloading tool..."
+
+curl -L https://raw.githubusercontent.com/DarkShadowUser/Jas-toolbox/main/jas -o $INSTALL_DIR/$SCRIPT_NAME
+
+chmod +x $INSTALL_DIR/$SCRIPT_NAME
+
+echo "[✓] Jas Toolbox installed!"
+echo "Run with: jas"
